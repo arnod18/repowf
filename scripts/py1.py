b91a94f5238995aea9866f52a3b4654210d3d776
arnod18 <42362536+arnod18@users.noreply.github.com>
2021-10-25 19:12:29 -0700

    Update py1.py

--- scripts/py1.py
+++ scripts/py1.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
-import sys
+import argparse, glob, json, re, subprocess, urllib.request, os, sys
+from sys import platform
+from pathlib import Path
 
 print(f'From python {sys.version_info}')
