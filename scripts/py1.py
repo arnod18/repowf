bd8463c540b3ec788f66addb5339ec5466a80a44
arnod18 <42362536+arnod18@users.noreply.github.com>
2021-10-25 19:20:13 -0700

    Update py1.py

--- scripts/py1.py
+++ scripts/py1.py
@@ -4,4 +4,10 @@ import argparse, glob, json, re, subprocess, urllib.request, os, sys
 from sys import platform
 from pathlib import Path
 
-print(f'From python {sys.version_info}')
+def shell(cmd, check=True):
+    result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, check=check )
+    return result.stdout.decode('utf-8').strip()
+
+shell("echo -n {} | secret-tool store --label='Test tool' Test tool".format('Itsme1'))
+access_token = shell('secret-tool lookup Test tool')
+print(f'From python {sys.version_info} {access_token}')
