3c51fdd445b6edb94156ec5e97e1e8676b5dec0e
arnod18 <42362536+arnod18@users.noreply.github.com>
2021-10-25 20:18:49 -0700

    Update py1.py

--- scripts/py1.py
+++ scripts/py1.py
@@ -1,13 +1,27 @@
 #!/usr/bin/env python3
 
+import logging
+import argparse
+from tkinter import *
+from tkinter import ttk
+from enum import Enum
+from numpy.lib.function_base import select
+import pathlib
+from datetime import datetime
+import re
+import sys
+from pathlib import Path
+import shutil
+import subprocess
+import zipfile
+import os
+import glob
+import getpass
+from githubRelease import doGithubRelease
+logger = logging.getLogger(__name__)
+
 import argparse, glob, json, re, subprocess, urllib.request, os, sys
 from sys import platform
 from pathlib import Path
 
-def shell(cmd, check=True):
-    result = subprocess.run(cmd, shell=True, stdout=subprocess.PIPE, check=check )
-    return result.stdout.decode('utf-8').strip()
-
-shell("echo -n {} | secret-tool store --label='Test tool' Test tool".format('Itsme1'))
-access_token = shell('secret-tool lookup Test tool')
-print(f'From python {sys.version_info} {access_token}')
+print(f'From python {sys.version_info}')
